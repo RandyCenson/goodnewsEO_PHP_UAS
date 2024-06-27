18774d181de1c90713a1ee0b0693186bd068b088
wil07aja <111937223+wil07aja@users.noreply.github.com>
2024-06-28 02:06:39 +0700

    Update Status.php

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,12 +6,16 @@ use App\Models\Order;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+// Mendefinisikan class Status yang merupakan model Eloquent
 class Status extends Model
 {
-    use HasFactory;
+    use HasFactory; // Menggunakan trait HasFactory untuk mendukung pembuatan instance model dengan factory
 
+    // Mengatur atribut yang tidak boleh diisi secara massal (mass assignment)
     protected $guarded = ['id'];
 
+    // Mendefinisikan relasi one-to-many dengan model Order
+    // Satu status dapat memiliki banyak order
     public function order()
     {
         return $this->hasMany(Order::class);
